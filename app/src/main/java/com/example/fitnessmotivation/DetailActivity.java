ec254449c3cf461391b5db0e2dbc8aa2d7722b05
sena-yun <2020m091@kuas.ac.jp>
2020-07-15 00:55:03 +0900

    Added YoutubeBP

--- app/src/main/java/com/example/fitnessmotivation/DetailActivity.java
+++ app/src/main/java/com/example/fitnessmotivation/DetailActivity.java
@@ -2,13 +2,32 @@ package com.example.fitnessmotivation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class DetailActivity extends AppCompatActivity {
+    private ImageView button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        button2 = (ImageView) findViewById(R.id.button2);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openYoutubeBP();
+            }
+        });
+    }
+
+    public void openYoutubeBP() {
+        Intent intent = new Intent(this, YoutubeBP.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/fitnessmotivation/MainActivity.java
+++ app/src/main/java/com/example/fitnessmotivation/MainActivity.java
@@ -2,19 +2,51 @@ package com.example.fitnessmotivation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private Button button;
+    private FloatingActionButton floatingActionButton4;
+    private FloatingActionButton floatingActionButton6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calender);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDetailActivity();
+            }
+        });
+
+        floatingActionButton4 = findViewById(R.id.floatingActionButton4);
+        floatingActionButton4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRecordActivity();
+            }
+        });
+
+        floatingActionButton6 = findViewById(R.id.floatingActionButton6);
+        floatingActionButton6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openCalenderActivity();
+            }
+        });
 
         Spinner spinner = findViewById(R.id.spinner);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Good,android.R.layout.simple_spinner_item);
@@ -32,5 +64,19 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+    public void openDetailActivity () {
+        Intent intent = new Intent(this,DetailActivity.class);
+        startActivity(intent);
+    }
+
+    public void openRecordActivity(){
+        Intent intent = new Intent(this,RecordActivity.class);
+        startActivity(intent);
+    }
+
+    public void openCalenderActivity(){
+        Intent intent = new Intent(this,CalenderActivity.class);
+        startActivity(intent);
     }
 }
